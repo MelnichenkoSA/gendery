22e4b7dc9a14991f0debbef4c4c975d19e9385c9
melnichenkosa <melnichenkosa@310D-4>
2022-12-03 09:05:04 +0700

    Добавьте файлы проекта.

---
+++ gendery/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace gendery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                int N = int.Parse(Console.ReadLine());
+
+                if ((N & (int)ДеньНедели.Вторник) == (int)ДеньНедели.Вторник || (N & (int)ДеньНедели.Среда) == (int)ДеньНедели.Среда)
+                    Console.WriteLine("Среда или Вторник");
+            }
+            
+        }
+    }
+    enum ДеньНедели
+    {
+        Понедельник = 1,
+        Вторник = 2,
+        Среда = 4,
+        Четверг = 8,
+        Пятница = 16,
+        Суббота = 32,
+        Воскресенье = 64
+
+    }
+
+}
